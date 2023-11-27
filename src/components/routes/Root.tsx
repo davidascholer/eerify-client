017bee191fb902a20471fbf368bd6836d8ac6119
David Scholer <david@nimbio.com>
2023-11-26 16:58:44 -0800

    root directs to home

--- src/components/routes/Root.tsx
+++ src/components/routes/Root.tsx
@@ -1,8 +1,17 @@
 import { Outlet } from "react-router-dom";
 import NavBar from "../navbar/NavBar"
+import PATHS from "../../router/paths";
+import { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
 
 export default function Root() {
-    
+
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    navigate(PATHS.HOME);
+  }, [navigate]);
+
     return (
       <>
         <NavBar/>
