72ae24c62bef11a9b3d01412ca3337c27d00cbaf
David Scholer <david@nimbio.com>
2024-01-18 04:23:36 -0800

    bug fix

--- src/components/mortgage/Results.tsx
+++ src/components/mortgage/Results.tsx
@@ -1,6 +1,6 @@
 import { Box, Typography } from "@mui/material";
 import BasicTextInput from "./BasicTextInput";
-import { ResultType } from "../App";
+import { ResultType } from "./Mortgage";
 
 function Results({
   monthlyMortgage,
