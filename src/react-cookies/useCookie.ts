4431bd75f47df6a5ab67e14f368b5acd25a94d8c
David Scholer <david@nimbio.com>
2024-03-29 01:59:16 -0700

    edited useCookie hook

--- src/react-cookies/useCookie.ts
+++ src/react-cookies/useCookie.ts
@@ -1,3 +1,17 @@
+/*
+yarn add js-cookie
+yarn add -D @types/js-cookie
+example use:
+  // Inside of component
+  const authCookie = useCookie("app-auth");
+  const refreshCookie = useCookie("app-refresh");
+  // Get value
+  authCookie.value;
+  // Set value
+  authCookie.set("auth-token");
+  // Refresh value in hook (useful for when cookie is updated outside of hook)
+  authCookie.refresh();
+*/
 import Cookies from "js-cookie";
 import { useEffect, useRef, useState } from "react";
 
