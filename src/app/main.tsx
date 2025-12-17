be03adc37de1466a23e26a0d22e300bee294d115
David Scholer <davidascholer@gmail.com>
2025-12-16 23:02:49 -0800

    tailwind refactor

--- src/app/main.tsx
+++ src/app/main.tsx
@@ -2,9 +2,11 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
+import { ErrorBoundary } from "react-error-boundary";
+import { ErrorFallback } from "./ErrorFallback";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
-  <React.StrictMode>
+  <ErrorBoundary FallbackComponent={ErrorFallback}>
     <App />
-  </React.StrictMode>
+  </ErrorBoundary>
 );
